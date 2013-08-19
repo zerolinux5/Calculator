47fdf201837fda35b3ff4ff97336d5eed08389d2
Jesus Magana <maganazero5@gmail.com>
2013-08-18 18:04:17 -0700

    added the masks for the buttons

--- gen/com/zerolinux5/calculator/R.java
+++ gen/com/zerolinux5/calculator/R.java
@@ -21,10 +21,52 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int eight=0x7f020000;
+        public static final int eightm=0x7f020001;
+        public static final int eightx=0x7f020002;
+        public static final int five=0x7f020003;
+        public static final int fivem=0x7f020004;
+        public static final int fivex=0x7f020005;
+        public static final int four=0x7f020006;
+        public static final int fourm=0x7f020007;
+        public static final int fourx=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int nine=0x7f02000a;
+        public static final int ninem=0x7f02000b;
+        public static final int ninex=0x7f02000c;
+        public static final int one=0x7f02000d;
+        public static final int onem=0x7f02000e;
+        public static final int onex=0x7f02000f;
+        public static final int seven=0x7f020010;
+        public static final int sevenm=0x7f020011;
+        public static final int sevenx=0x7f020012;
+        public static final int six=0x7f020013;
+        public static final int sixm=0x7f020014;
+        public static final int sixx=0x7f020015;
+        public static final int three=0x7f020016;
+        public static final int threem=0x7f020017;
+        public static final int threex=0x7f020018;
+        public static final int two=0x7f020019;
+        public static final int twom=0x7f02001a;
+        public static final int twox=0x7f02001b;
+        public static final int zero=0x7f02001c;
+        public static final int zerom=0x7f02001d;
+        public static final int zerox=0x7f02001e;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000c;
+        public static final int button1=0x7f080000;
+        public static final int button10=0x7f08000a;
+        public static final int button11=0x7f080009;
+        public static final int button12=0x7f08000b;
+        public static final int button2=0x7f080001;
+        public static final int button3=0x7f080002;
+        public static final int button4=0x7f080003;
+        public static final int button5=0x7f080004;
+        public static final int button6=0x7f080005;
+        public static final int button7=0x7f080006;
+        public static final int button8=0x7f080007;
+        public static final int button9=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +78,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int number=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/zerolinux5/calculator/MainActivity.java
+++ src/com/zerolinux5/calculator/MainActivity.java
@@ -2,7 +2,9 @@ package com.zerolinux5.calculator;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,30 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Create the buttons
+		Button one = (Button)findViewById(R.id.button11);
+		Button two = (Button)findViewById(R.id.button6);
+		Button three = (Button)findViewById(R.id.button8);
+		Button four = (Button)findViewById(R.id.button12);
+		Button five = (Button)findViewById(R.id.button7);
+		Button six = (Button)findViewById(R.id.button9);
+		Button seven = (Button)findViewById(R.id.button3);
+		Button eight = (Button)findViewById(R.id.button2);
+		Button nine = (Button)findViewById(R.id.button4);
+		Button zero = (Button)findViewById(R.id.button5);
+		
+		//Give each button pictures
+		one.setBackgroundResource(R.drawable.onex);
+		two.setBackgroundResource(R.drawable.twox);
+		three.setBackgroundResource(R.drawable.threex);
+		four.setBackgroundResource(R.drawable.fourx);
+		five.setBackgroundResource(R.drawable.fivex);
+		six.setBackgroundResource(R.drawable.sixx);
+		seven.setBackgroundResource(R.drawable.sevenx);
+		eight.setBackgroundResource(R.drawable.eightx);
+		nine.setBackgroundResource(R.drawable.ninex);
+		zero.setBackgroundResource(R.drawable.zerox);
 	}
 
 	@Override
@@ -17,6 +43,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig){
+		//ignore orientation/keyboard change
+		super.onConfigurationChanged(newConfig);
 	}
 
 }
